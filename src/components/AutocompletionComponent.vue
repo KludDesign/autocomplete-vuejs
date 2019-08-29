95217119f8564b7c0011f76d52b761b09e73c7bc
klud <vincentgillibert@gmail.com>
2019-08-29 17:36:11 +0200

    v-for on data to get the contry name

--- src/components/AutocompletionComponent.vue
+++ src/components/AutocompletionComponent.vue
@@ -1,32 +1,44 @@
-<template lang="html">
-  <div class="">
+<template>
+  <div>
     <h1>Autocompletion component</h1>
-    <p>{{ contries }}</p>
+    <div v-for="contryData in contriesData">
+      <p>{{ contryData.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import RestcountriesApiService from '@/services/RestcountriesApiService'
+import RestcountriesApi from '@/services/RestcountriesApi'
 
 export default {
   data () {
     return {
-      contries: []
+      contriesData: null
     }
   },
 
   mounted () {
-    this.getContries()
+    this.getContriesData()
   },
 
   methods: {
-    async getContries () {
-      const response = await RestcountriesApiService.getAllContries()
-      this.contries = response.data
+    // Get all the countries information from the API and put in
+    // the contriesData variable
+    async getContriesData () {
+      const response = await RestcountriesApi.getAllContries()
+      this.contriesData = response.data
     }
   }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+
+$text-color: #94bbdc;
+$background-color: #f2f5f7;
+
+h1 {
+  color: $text-color;
+}
+
 </style>
--- src/services/RestcountriesApi.js
+++ src/services/RestcountriesApi.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+// The restcountries API Endpoint
 const RESTCOUNTRIES_API_URL = 'https://restcountries.eu/rest/v2';
 
 export default {
+  // Add a get call from the API to get all countries
   getAllContries () {
     return axios.get(`${RESTCOUNTRIES_API_URL}/all`)
   }
